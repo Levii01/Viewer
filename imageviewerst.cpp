eb5c090f39eeda558a1d10d1c439893f8a1fb9d5
Levii01 <m.sedziak01@gmail.com>
2017-09-06 13:30:25 +0200

    Set properties for scrollArea and imageLabel

--- imageviewerst.cpp
+++ imageviewerst.cpp
@@ -7,8 +7,16 @@ ImageViewerST::ImageViewerST(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    ui->imageLabel->setBackgroundRole(QPalette::Base);
+    ui->imageLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+    ui->imageLabel->setScaledContents(true); // full view
+
+    ui->scrollArea->setBackgroundRole(QPalette::Dark);
+
     QImage image("/Users/emsi/Desktop/image.png");
     ui->imageLabel->setPixmap(QPixmap::fromImage(image));
+
+    setWindowTitle(tr("Image Viewer Project"));
 }
 
 ImageViewerST::~ImageViewerST()
